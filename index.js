a3fb1be9264db12afb752e03f3e57a36f236404f
Misha Marinenko <0x77dev@protonmail.com>
2019-12-21 00:22:26 +0200

    #3 #4 #5 / parse Battery values / API Added / Documentation

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const iQOS = require("./lib/iqos");
+const express = require("express");
+
 const iqos = new iQOS();
+const api = express();
+
+api.get("/battery", (req, res) => {
+	res.json(iqos.battery);
+});
+
+api.listen(1447);
