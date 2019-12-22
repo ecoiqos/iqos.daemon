f90b3eb68555dacbe0ffea3032aee1891bd25fa4
Misha Marinenko <0x77dev@protonmail.com>
2019-12-22 15:20:26 +0200

    temproary solution

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 
 const iqos = new iQOS();
 const api = express();
+if (process.env.REPL) {
+	const cli = require("repl").start("> ");
+	cli.context.iqos = iqos;
+}
 
 api.get("/battery", (req, res) => {
 	res.json(iqos.battery);
